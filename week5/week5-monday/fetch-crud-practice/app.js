3fe6edeebe2f78ff723f5645a67de8958f9c410c
Minoo Yaghoubi <minoo.yaghoubi@gmail.com>
2026-02-09 11:18:03 +0200

    add create blog using fetch POST

--- week5/week5-monday/fetch-crud-practice/app.js
+++ week5/week5-monday/fetch-crud-practice/app.js
@@ -1,8 +1,23 @@
+// app.js
 const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
-const testFech = async () => {
-  const res = await fetch(apiUrl);
-  const data = await res.json();
-  console.log(data.length);
-}
 
-testFech();
+const blog = {
+  title: 'My First Blog',
+  body: 'Hello from fetch!',
+  userId: 1,
+};
+
+const createBlog = async () => {
+  const res = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(blog),
+  });
+
+  const data = await res.json();
+  console.log('CREATED:', data);
+};
+
+createBlog();
