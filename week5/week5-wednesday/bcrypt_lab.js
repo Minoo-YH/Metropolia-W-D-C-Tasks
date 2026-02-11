c77dcba49d2dd4b0a428aa054ac6baf66b69fc31
Minoo Yaghoubi <minoo.yaghoubi@gmail.com>
2026-02-11 11:19:49 +0200

    feat: step2 hash password and parse bcrypt hash format

--- week5/week5-wednesday/bcrypt_lab.js
+++ week5/week5-wednesday/bcrypt_lab.js
@@ -1,19 +1,23 @@
 const bcrypt = require('bcrypt');
 
 
-async function step1_showSalt() {
-   const cost = 10;
-    const salt = await bcrypt.genSalt(10);
+async function step2_hashPassword() {
+   const password = "mySecurePassword";
+    const cost = 10;
 
-  console.log("=== Step 1: Salt ===");
-  console.log("Cost factor:", cost);
+    const salt = await bcrypt.genSalt(cost);
+  const hash = await bcrypt.hash(password, salt);
+
+console.log("\n=== Step 2: Hash ===");
+  console.log("Plain password:", password);
   console.log("Salt:", salt);
+  console.log("Hash:", hash);
 
-const parts= salt.split('$');
-console.log("Parsed salt => version:", parts[1], "| cost:", parts[2]);
+const hashParts = hash.split("$");
+  console.log("Parsed hash => version:", hashParts[1], "| cost:", hashParts[2]);
+  console.log("Salt+hash payload length:", hashParts[3].length);
 }
-
-step1_showSalt().catch(console.error);
+step2_hashPassword().catch(console.error);
 
 
 
