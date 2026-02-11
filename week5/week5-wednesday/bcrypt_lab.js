608d3c090169c2a2cbd5a2b5391c88834113e039
Minoo Yaghoubi <minoo.yaghoubi@gmail.com>
2026-02-11 11:15:41 +0200

    feat: step1 generate and explain bcrypt salt

--- week5/week5-wednesday/bcrypt_lab.js
+++ week5/week5-wednesday/bcrypt_lab.js
@@ -1,4 +1,33 @@
 const bcrypt = require('bcrypt');
 
 
-console.log("bcrypt loaded successfully");
+async function step1_showSalt() {
+   const cost = 10;
+    const salt = await bcrypt.genSalt(10);
+
+  console.log("=== Step 1: Salt ===");
+  console.log("Cost factor:", cost);
+  console.log("Salt:", salt);
+
+const parts= salt.split('$');
+console.log("Parsed salt => version:", parts[1], "| cost:", parts[2]);
+}
+
+step1_showSalt().catch(console.error);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log("bcrypt loaded successfully");
